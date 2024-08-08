d0ba7bbdac3b31dd6efb6612299cf85d50ca36cc
DomenicoTe <domenicot1997@gmail.com>
2024-08-08 12:44:37 +0200

    Update

--- libs/mongo/index.js
+++ libs/mongo/index.js
@@ -2,8 +2,9 @@ const exec = require('../utils/exec')
 const { quiet } = require('../../config')
 module.exports = async function (path, url, db = 'agile-factory') {
     try {
-        //Non voglio i log di mongodump
-        await exec(`mongodump --host ${url} --port 27017 --archive=${path}/mongo.gz --gzip --db ${db} ${quiet ? '--quiet' : ''}`)
+        if(!quiet) console.log('Dumping Mongo')
+        if(!quiet) await exec(`mongodump --host ${url} --port 27017 --archive=${path}/mongo.gz --gzip --db ${db} --quiet`)
+        else await exec(`mongodump --host ${url} --port 27017 --archive=${path}/mongo.gz --gzip --db ${db}`)
         console.log('Mongo complete')
     }
     catch (e) { console.log("Mongo failed") }
