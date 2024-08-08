501675aa5e1d2b63481efeb3078bccc41d1c5574
DomenicoTe <domenicot1997@gmail.com>
2024-08-08 12:39:49 +0200

    !Commit

--- libs/utils/exec.js
+++ libs/utils/exec.js
@@ -1,6 +1,7 @@
 const { exec } = require('child_process');
-
+const { quiet } = require('../../config')
 module.exports = function (command) {
+    if (!quiet) console.log(command)
     return new Promise((resolve, reject) => {
         exec(command, (error, stdout, stderr) => {
             if (error) reject(error)
