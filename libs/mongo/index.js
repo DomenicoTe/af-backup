89272dcfc427cc4386124748c8f48d3076781307
DomenicoTe <domenicot1997@gmail.com>
2024-08-08 13:04:17 +0200

    Commit

--- libs/mongo/index.js
+++ libs/mongo/index.js
@@ -3,7 +3,7 @@ const { quiet } = require('../../config')
 module.exports = async function (path, url, db = 'agile-factory') {
     try {
         if(!quiet) console.log('Dumping Mongo')
-        if(!quiet) await exec(`mongodump --host ${url} --port 27017 --archive=${path}/mongo.gz --gzip --db ${db} --quiet`)
+        if(quiet) await exec(`mongodump --host ${url} --port 27017 --archive=${path}/mongo.gz --gzip --db ${db} --quiet`)
         else await exec(`mongodump --host ${url} --port 27017 --archive=${path}/mongo.gz --gzip --db ${db}`)
         console.log('Mongo complete')
     }
