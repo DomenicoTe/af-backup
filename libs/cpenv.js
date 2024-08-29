e561269e5c0ba78454296e668ecebaab52e6b312
DomenicoTe <domenicot1997@gmail.com>
2024-08-29 16:03:07 +0200

    1.5.0

--- libs/cpenv.js
+++ libs/cpenv.js
@@ -5,7 +5,9 @@ module.exports = function (dir, environment) {
     const files = fs.readdirSync(environment).filter(file => file.endsWith('.env'))
     if (files.length === 0) { console.log('No .env files found'); return false }
     // files.forEach(file => fs.copyFileSync(path.join(environment, file), path.join(dir, file)))
-    for (const file of files)fs.copyFileSync(path.join(environment, file), path.join(dir, file))
+    for (const file of files) fs.copyFileSync(path.join(environment, file), path.join(dir, file))
+    const opcua = fs.readdirSync(path.join(environment, "bin"))[0]
+    fs.copyFileSync(path.join(environment, "bin", opcua), path.join(dir, opcua))
     return true
 
 }
