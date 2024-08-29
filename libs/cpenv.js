1f3e5143119ac7d7ea544eed7f50c2d515f2e272
DomenicoTe <domenicot1997@gmail.com>
2024-08-29 15:53:25 +0200

    chore: Remove unnecessary console.log statements in cpenv.js

--- libs/cpenv.js
+++ libs/cpenv.js
@@ -5,7 +5,7 @@ module.exports = function (dir, environment) {
     const files = fs.readdirSync(environment).filter(file => file.endsWith('.env'))
     if (files.length === 0) { console.log('No .env files found'); return false }
     // files.forEach(file => fs.copyFileSync(path.join(environment, file), path.join(dir, file)))
-    for (const file of files) { console.log("Coping:", file, "to:", dir); fs.copyFileSync(path.join(environment, file), path.join(dir, file)) }
+    for (const file of files)fs.copyFileSync(path.join(environment, file), path.join(dir, file))
     return true
 
 }
