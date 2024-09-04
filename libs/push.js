26abbb76e2b2fb97906fdf9d908d4bef2e9db85f
DomenicoTe <domenicot1997@gmail.com>
2024-09-04 12:23:30 +0200

    vda2330

--- libs/push.js
+++ libs/push.js
@@ -4,10 +4,10 @@ setImmediate(async () => {
     //Esegui il commit di tutti i file specificando solo il nome della versione come commento
     await exec('git add .').finally(() => console.log('Added all files to commit'));
     await exec('git commit -am "v' + version + '"').finally(() => console.log('Committed all files'));
-    await exec('git push').catch((e) => console.error(e));
-    await exec(`git tag -d ${version}`).catch((e) => console.error(e))
-    await exec(`git tag ${version}`).catch((e) => console.error(e));// Se il tag esiste già rimuovilo
+    await exec('git push').catch((e) => console.log(e));
+    await exec(`git tag -d ${version}`).catch((e) => console.log(e))
+    await exec(`git tag ${version}`).catch((e) => console.log(e));// Se il tag esiste già rimuovilo
 
+    await exec(`git push origin ${version}`).catch((e) => console.log(e));
 
-    await exec(`git push origin ${version}`).catch((e) => console.error(e));
 })
