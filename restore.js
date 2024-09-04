d53748f9ea3f42ed9c117e1e8acb89640f89fd85
DomenicoTe <domenicot1997@gmail.com>
2024-09-04 16:07:20 +0200

    vd.0.66

--- restore.js
+++ restore.js
@@ -4,7 +4,7 @@ const exec = require('./libs/utils/main').exec
 setImmediate(async () => { await get(process.argv[2]) })
 async function get(date) {
     if (!date) throw new Error('Date not provided for restore')
-    console.log(new Date(), 'Restore started')
+    console.log(new Date(), 'Restore started', require('./package.json').version)
     await ftpGet(date, config.environment, config.ftp).catch(e => console.log(e.toString()))
     await exec(`tar -xjf ${config.environment}/${date}.tar.bz2`).catch(e => console.log(e.toString()))
     // await exec(`rm ${config.environment}/${date}.tar.bz2`).catch(e => console.log(e.toString()))
