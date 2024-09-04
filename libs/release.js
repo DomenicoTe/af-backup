b04d60e509860dfaa8b7935455124c45c7429aa3
DomenicoTe <domenicot1997@gmail.com>
2024-09-04 13:11:20 +0200

    v1.7.1

--- libs/release.js
+++ libs/release.js
@@ -1,4 +1,19 @@
-const exec = require('./utils/main').exec;
-setImmediate(async () => {
-    await exec("yarn new " + process.argv[2] + " yarn build && yarn push").catch((e) => console.log(e.toString())).finally(() => console.log('Released all changes'));
-})
+const exec = require('child_process').exec;
+const command_new = "yarn new " + process.argv[2];
+const command_build = "yarn build";
+const command_push = "yarn push";
+exec(command_new, (error, stdout, stderr) => {
+    console.log("Running: " + command_new);
+    if (error) { console.error(`exec error: ${error}`); return; }
+    console.log(stdout);
+});
+exec(command_build, (error, stdout, stderr) => {
+    console.log("Running: " + command_build);
+    if (error) { console.error(`exec error: ${error}`); return; }
+    console.log(stdout);
+});
+exec(command_push, (error, stdout, stderr) => {
+    console.log("Running: " + command_push);
+    if (error) { console.error(`exec error: ${error}`); return; }
+    console.log(stdout);
+});
