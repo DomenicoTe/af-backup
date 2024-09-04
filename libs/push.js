df1fd89e09b4a608715985f85c3144585fac0c48
DomenicoTe <domenicot1997@gmail.com>
2024-09-04 12:34:31 +0200

    vD.0.e0b

--- libs/push.js
+++ libs/push.js
@@ -2,14 +2,12 @@ const exec = require('./utils/main').exec;
 const version = require('../package.json').version;
 setImmediate(async () => {
     //Esegui il commit di tutti i file specificando solo il nome della versione come commento
-    await exec('git add .').finally(() => console.log('Added all files to commit'));
-    await exec('git commit -am ' + version + '"').finally(() => console.log('Committed all files'));
-    await exec('git push').catch((e) => console.log(e));
-    await exec(`git tag -d ${version}`).catch((e) => console.log(e))
-    await exec(`git tag ${version}`).catch((e) => console.log(e));// Se il tag esiste già rimuovilo
-    //Updates were rejected because the tag already exists in the remote.
-    // Se il tag remoto esiste già rimuovilo
-    await exec(`git push origin :refs/tags/${version}`).catch((e) => console.log(e));
-    await exec(`git push origin ${version}`).catch((e) => console.log(e));
+    await exec('git add .').catch((e) => console.log(e.toString())).finally(() => console.log('Added all files to commit'));
+    await exec('git commit -am ' + version + '"').catch((e) => console.log(e.toString())).finally(() => console.log('Committed all files'));
+    await exec('git push').catch((e) => console.log(e.toString())).finally(() => console.log('Pushed all changes'));
+    await exec(`git tag -d ${version}`).catch((e) => console.log(e.toString()))
+    await exec(`git tag ${version}`).catch((e) => console.log(e.toString())).finally(() => console.log('Tagged all changes'));
+    await exec(`git push origin :refs/tags/${version}`).catch((e) => console.log(e.toString()))
+    await exec(`git push origin ${version}`).catch((e) => console.log(e.toString())).finally(() => console.log('Pushed all changes'));
 
 })
--- libs/version.js
+++ libs/version.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const package = require('../package.json');
 //D.0. numero di 3 cifre esadecimale
-package.version = process.argv[2] ? "v" + process.argv[2] : "D.0." + Math.floor(Math.random() * 0xFFF).toString(16);
+package.version = process.argv[2] ? "v" + process.argv[2] : "vD.0." + Math.floor(Math.random() * 0xFFF).toString(16);
 fs.writeFileSync('./package.json', JSON.stringify(package, null, 2));
 console.log('Version updated to ' + package.version);
