130f4b0ee2c653cbf8bbc1dbad15688c6abfa099
DomenicoTe <domenicot1997@gmail.com>
2024-09-04 12:19:41 +0200

    vda2330

--- libs/push.js
+++ libs/push.js
@@ -4,6 +4,7 @@ setImmediate(async () => {
     //Esegui il commit di tutti i file specificando solo il nome della versione come commento
     await exec('git add .');
     await exec('git commit -am "v' + version + '"');
+    await exec('git push');
     // await exec(`git tag ${version}`);
     // await exec(`git push origin ${version}`);
 })
