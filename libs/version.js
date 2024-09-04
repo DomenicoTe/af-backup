3d0829ce8ac467a0ca776a17548aade684bbb61b
DomenicoTe <domenicot1997@gmail.com>
2024-09-04 12:17:58 +0200

    vda2330

--- libs/version.js
+++ libs/version.js
@@ -1,8 +1,4 @@
 const fs = require('fs');
 const package = require('../package.json');
-//Modifica il file package.json con la versione corrente come process argv
-package.version = process.argv[2];
-//salva il file package.json
+package.version = process.argv[2] ? "v" + process.argv[2] : Math.random().toString(16).slice(2, 8);
 fs.writeFileSync('./package.json', JSON.stringify(package, null, 2));
-console.log("git tag v" + package.version);
-console.log("git push origin v" + package.version);
