2c2e2e128f4e4aa27419afa32ef65f52e9706195
DomenicoTe <domenicot1997@gmail.com>
2024-09-04 12:19:11 +0200

    ciao

---
+++ libs/push.js
@@ -0,0 +1,9 @@
+const exec = require('./utils/main').exec;
+const version = require('../package.json').version;
+setImmediate(async () => {
+    //Esegui il commit di tutti i file specificando solo il nome della versione come commento
+    await exec('git add .');
+    await exec('git commit -am "v' + version + '"');
+    // await exec(`git tag ${version}`);
+    // await exec(`git push origin ${version}`);
+})
