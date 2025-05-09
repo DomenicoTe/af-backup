c22cc90039aa957aae8bf325cc296e7dafd4decd
DomenicoTe <domenicot1997@gmail.com>
2025-05-09 18:09:44 +0200

    v3.0.0

--- .yarn/push.js
+++ .yarn/push.js
@@ -5,7 +5,7 @@ setImmediate(async () => {
         await execute('git add .'); console.log('Added all files to commit')
         await execute(`git commit -am "${version}"`); console.log('Committed all files')
         await execute('git push'); console.log('Pushed all changes')
-        await execute(`git tag -d ${version}`); console.log('Deleted old tag')
+        await execute(`git tag -d ${version}`).catch((e)=>{console.log("NoDelete")}); console.log('Deleted old tag')
         await execute(`git tag ${version}`); console.log('Tagged all changes')
         await execute(`git push origin :refs/tags/${version}`); console.log('Deleted old tag from remote')
         await execute(`git push origin ${version}`); console.log('Pushed all changes')
