659f6cb8fa84ebd0c400885005efc5d696e535f6
DomenicoTe <domenicot1997@gmail.com>
2025-05-13 14:47:50 +0200

    v3.1.2

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const main = require('./src/index.js');
 console.debug = require('./src/libs/utils').debug
-const scheduler = require('./src/libs/utils')
+const { scheduler } = require('./src/libs/utils')
+const { version } = require('./package.json');
+console.debug.log(`Agile Factory Backup ${version} started`);
+
 scheduler(main)
